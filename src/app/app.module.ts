4c149e5f2409b069ebb652861e047b1e66b07aca
cherryApp <j.cserko@gmail.com>
2023-05-13 12:24:58 +0200

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SortPipe } from './pipe/sort.pipe';
 import { PricingPageComponent } from './pricing-page/pricing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PricingPageComponent,
+    SortPipe,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
